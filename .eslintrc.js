8cb4408621db173607a36b7b1eaf9c6a2cd939c4
Camryn Reid <camrr1991@gmail.com>
2019-12-21 13:43:42 -0800

    Fixes bugs in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js',
-  extends: 'aribnb',
-  env: {
-    node: true,
-    browser: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      "jsx": true
+    },
+    plugins: [ "react" ],
+    extends: {
+      eslint:"recommended",
+      plugin:"react/recommended"
+    }
   }
-};
+}
